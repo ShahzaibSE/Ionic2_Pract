94bff81d7372d143e7d8d62c3d3a20a0afde9475
Shahzaibanalyst <shahzaibanalyst@outlook.com>
2016-04-13 19:36:06 +0500

    Commit 13_4_16

--- plugin_Pract/app/pages/home/home.ts
+++ plugin_Pract/app/pages/home/home.ts
@@ -1,14 +1,21 @@
 import {Page,Alert,NavController} from 'ionic-angular';
-import {Camera,Device} from 'ionic-native';
+import {Camera} from 'ionic-native';
+import {Device} from 'ionic-native';
+import {Geolocation} from 'ionic-native';
+
+//Components.
+import {Map} from './../map/map';
 
 enum Days {'Day1','Day2'};
+
+console.log(Days['Day1']);
 
 @Page({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
   
-  ImageData:any;  
+  ImageData; 
     
   constructor(public nav:NavController) {
 
@@ -16,11 +23,16 @@ export class HomePage {
 
   }
   
+  gotoMap()
+  {
+      this. nav.push(Map);
+  }
+  
   getPicture()
   {
-      let options= {
-          destination : Camera['DestinationType'].DATA_URL,
-      };
+      /*let options= {
+          destinationType : Camera['DestinationType'].DATA_URL,
+      };*/
       
       /*Camera.getPicture(options).then((ImageData) => {
          // imageData is either a base64 encoded string or a file URI
@@ -29,25 +41,90 @@ export class HomePage {
           }, (err) => {
          });*/
        
-       let alert  = Alert.create({
+       /*let alert  = Alert.create({
            title:"Picture Captured",
            buttons : ['ok']
        })
        
-       this.nav.present(alert);  
+       this.nav.present(alert); */ 
          
        console.log("Pictured Captured"); 
-      
-      Camera.getPicture(options).then((ImageData) => 
-      {
-         let base64Image = ImageData;
-         this.ImageData = base64Image;   
-      },(err)=>
-      {
-          console.log(err);
-      })  
+       
+       let option = {
+          //destinationType : Camera["DestinationType"].DATA_URL,
+          destinationType : Camera["destinationType"].DATA_URL,
+          allowEdit : false,
+          targetWidth : 50
+       }
+       
+       
+       //var destinationType = Camera.DestinationType.FILE_URI;     
+           
+       Camera.getPicture(option).then((imageData) => {
+        // imageData is either a base64 encoded string or a file URI
+        // If it's base64:
+        //"data:image/jpeg;base64," + 
+        //let base64Image = imageData;
+        let fileURI = imageData
+        this.ImageData = fileURI;
+        alert(fileURI);
+    
+    }, (err) => {
+    });  
          
        /*console.log(Days[0]);  
        alert(Days[0]);*/
+   }
+   
+   sentMessage()
+   {
+       let alert  = Alert.create(
+           {
+               title:"Do you want to send your message?",
+               buttons : [
+                   {
+                       text:'Yes',
+                       handler : ()=>
+                       {
+                           let alert1 = Alert.create({
+                               title:"Message Sent"
+                           })
+                           
+                           this.nav.present(alert1);
+                       }
+                   },
+                   {
+                       text:'No',
+                       handler : ()=>
+                       {
+                           let alert1 = Alert.create({
+                               title:"Exiting..."
+                           })
+                           
+                           this.nav.present(alert1);
+                       }
+                   }
+               ]
+           }
+       )
+       
+       this.nav.present(alert);
+       
+   }
+  
+   
+   View_DeviceStatus()
+   {
+       //let status : Device;
+       console.log(Device.device);
+       
+       /*let specifications = Alert.create(
+           {
+               title:status.model,
+               buttons : ['Ok']
+           }
+       )
+       
+       this.nav.present(specifications);*/
    }
 }
